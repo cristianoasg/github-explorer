b797bbb29bf435b66de297d7f4f19554225f575a
Cristiano Augusto <cristiano.asg@hotmail.com>
2021-03-23 22:00:52 +0000

    using useEfect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,15 +1,24 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
 
 const repository = {
   name: 'unform',
   description: 'Forms in React',
   link: 'https://github.com/unform/unform'
-
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/cristianoasg/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
